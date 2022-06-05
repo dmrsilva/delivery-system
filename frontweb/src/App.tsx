f352f46d1528856f3f42fcc4ebdd26c24723cb69
Daniel Silva <daniel.mascr@gmail.com>
2022-06-05 13:21:06 -0300

    Frontweb created and add Bootstrap

---
+++ frontweb/src/App.tsx
@@ -0,0 +1,12 @@
+import './assets/styles/custom.scss';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Delivery System</h1>
+    </div>
+  );
+}
+
+export default App;
