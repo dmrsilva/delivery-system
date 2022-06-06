9f54d706714c0e8923329d80ff148cefe90ce5f9
Daniel Silva <daniel.mascr@gmail.com>
2022-06-06 17:43:31 -0300

    Customized ProductDetails page title

--- frontweb/src/pages/Menu/index.tsx
+++ frontweb/src/pages/Menu/index.tsx
@@ -1,5 +1,8 @@
 import ProductCard from 'components/ProductCard';
+import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
+
+import './styles.css';
 
 const Menu = () => {
   const product: Product = {
@@ -12,22 +15,35 @@ const Menu = () => {
   };
 
   return (
-    <div className="container my-4">
+    <div className="container my-4 menu-container">
+      <div className="row menu-title-container">
+        <h1>Menu de Produtos</h1>
+      </div>
       <div className="row">
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product} />
+          <Link to="/products/1">
+            <ProductCard product={product} />
+          </Link>
         </div>
       </div>
     </div>
