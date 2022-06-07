32bb33956af7afa0ac3539a1b19e1a84d8b6b465
Daniel Silva <daniel.mascr@gmail.com>
2022-06-07 09:48:33 -0300

    Added structured and styled pagination

--- frontweb/src/pages/Menu/index.tsx
+++ frontweb/src/pages/Menu/index.tsx
@@ -1,3 +1,4 @@
+import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
@@ -46,6 +47,10 @@ const Menu = () => {
           </Link>
         </div>
       </div>
+      <div className="row">
+        <Pagination />
+      </div>
+
     </div>
   );
 };
