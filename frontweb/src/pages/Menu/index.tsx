37867a4e82dc18ec6f6a5afdc1aa9f14f4ca2542
Daniel Silva <daniel.mascr@gmail.com>
2022-06-07 16:37:55 -0300

    Added loaders on the ProductDetails page

--- frontweb/src/pages/Menu/index.tsx
+++ frontweb/src/pages/Menu/index.tsx
@@ -7,11 +7,13 @@ import { Product } from 'types/product';
 import { AxiosParams } from 'types/vendor/axios';
 import { SpringPage } from 'types/vendor/spring';
 import { BASE_URL } from 'utils/requests';
+import CardLoader from './CardLoader';
 
 import './styles.css';
 
 const Menu = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const params: AxiosParams = {
@@ -23,9 +25,14 @@ const Menu = () => {
       },
     };
 
-    axios(params).then((response) => {
-      setPage(response.data);
-    });
+    setIsLoading(true);
+    axios(params)
+      .then((response) => {
+        setPage(response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
   return (
@@ -34,13 +41,14 @@ const Menu = () => {
         <h1>Menu de Produtos</h1>
       </div>
       <div className="row">
-        {page?.content.map((product) => (
+        {isLoading ? <CardLoader /> : (
+          page?.content.map((product) => (
           <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
             <Link to="/products/1">
               <ProductCard product={product} />
             </Link>
           </div>
-        ))}
+        )))}
       </div>
       <div className="row">
         <Pagination />
