d6fb547f1a1379156ba7869db19085bfdb5ffb6d
Daniel Silva <daniel.mascr@gmail.com>
2022-06-08 16:23:14 -0300

    Route level permission

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -17,7 +17,7 @@ const Admin = () => {
           <PrivateRoute path="/admin/orders">
             <h1>Order CRUD</h1>
           </PrivateRoute>
-          <PrivateRoute path="/admin/users">
+          <PrivateRoute path="/admin/users" roles={['ROLE_ADMIN']}>
             <Users />
           </PrivateRoute>
         </Switch>
