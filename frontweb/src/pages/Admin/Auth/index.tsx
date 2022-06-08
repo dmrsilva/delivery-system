fefd2b7937f3e636d238725910e7f01918a3564a
Daniel Silva <daniel.mascr@gmail.com>
2022-06-07 21:50:36 -0300

    Login card styling

--- frontweb/src/pages/Admin/Auth/index.tsx
+++ frontweb/src/pages/Admin/Auth/index.tsx
@@ -1,18 +1,25 @@
 import AuthImage from 'assets/images/main-image.png';
 import { Route, Switch } from 'react-router-dom';
+import Login from '../Login';
+
+import './styles.css';
 
 const Auth = () => {
   return (
     <div className="auth-container">
       <div className="auth-banner-container">
         <h1>Gerencie seus pedidos no Delivery System</h1>
-        <p>A nossa solução é sinônimo de safistação para os seus clientes.</p>
+        <p>
+          A nossa solução é sinônimo de safistação para os seus clientes. Com o
+          Delivery System sua empresa gerencia produtos e pedidos em um só
+          lugar.
+        </p>
         <img src={AuthImage} alt="Entragador de motocicleta" />
       </div>
       <div className="auth-form-container">
         <Switch>
           <Route path="/admin/auth/login">
-            <h1>Card de Login</h1>
+            <Login />
           </Route>
           <Route path="/admin/auth/signup">
             <h1>Card de Signup</h1>
