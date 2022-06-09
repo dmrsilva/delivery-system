6889edaaf07aa888a1267a55c6c2eda0341b0756
Daniel Silva <daniel.mascr@gmail.com>
2022-06-09 10:26:35 -0300

    ProductCrudCard horizontal responsive

--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -14,17 +14,19 @@ const ProductCrudCard = ({ product }: Props) => {
         <img src={product.imgUri} alt={product.name} />
       </div>
 
-      <div className="product-crud-card-bottom-container">
-        <h6>{product.name}</h6>
-        <ProductPrice price={product.price} />
+      <div className="product-crud-card-description">
+        <div className="product-crud-card-bottom-container">
+          <h6>{product.name}</h6>
+          <ProductPrice price={product.price} />
+        </div>
       </div>
 
       <div className="product-crud-card-buttons-container">
-        <button className="btn btn-outline-danger product-crud-card-button">
-            EXCLUIR
+        <button className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first">
+          EXCLUIR
         </button>
         <button className="btn btn-outline-secondary product-crud-card-button">
-            EDITAR
+          EDITAR
         </button>
       </div>
     </div>
