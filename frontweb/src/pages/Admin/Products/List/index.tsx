b88ac1471a79a7dfff3938b8b5a9362556c409f8
Daniel Silva <daniel.mascr@gmail.com>
2022-06-09 09:12:48 -0300

    Product list basic layout

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,6 +1,41 @@
+import ProductCrudCard from 'components/ProductCrudCard';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
 const List = () => {
-    return <h1>Tela List</h1>;
+  const product = {
+    id: 1,
+    name: 'Hamburger',
+    description: 'Hamburger de siri',
+    price: 35.0,
+    imgUri:
+      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
   };
-  
-  export default List;
-  
+
+  return (
+    <>
+      <div className="product-crud-bar-container">
+        <Link to="/admin/products/create">
+          <button className="btn btn-primary text-white btn-crud-add">
+            ADICIONAR
+          </button>
+        </Link>
+        <div className="base-card product-filter-container">Search bar</div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6 col-md-12">
+          <ProductCrudCard product={product} />
+        </div>
+        <div className="col-sm-6 col-md-12">
+          <ProductCrudCard product={product} />
+        </div>
+        <div className="col-sm-6 col-md-12">
+          <ProductCrudCard product={product} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default List;
