0458beda15aa62824d30578e98c1f6e884df5271
Daniel Silva <daniel.mascr@gmail.com>
2022-06-09 09:53:16 -0300

    Finishing ProductCrudCard mobile layout

---
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -0,0 +1,34 @@
+import ProductPrice from 'components/ProductPrice';
+import { Product } from 'types/product';
+
+import './styles.css';
+
+type Props = {
+  product: Product;
+};
+
+const ProductCrudCard = ({ product }: Props) => {
+  return (
+    <div className="base-card product-crud-card">
+      <div className="product-crud-card-top-container">
+        <img src={product.imgUri} alt={product.name} />
+      </div>
+
+      <div className="product-crud-card-bottom-container">
+        <h6>{product.name}</h6>
+        <ProductPrice price={product.price} />
+      </div>
+
+      <div className="product-crud-card-buttons-container">
+        <button className="btn btn-outline-danger product-crud-card-button">
+            EXCLUIR
+        </button>
+        <button className="btn btn-outline-secondary product-crud-card-button">
+            EDITAR
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCrudCard;
