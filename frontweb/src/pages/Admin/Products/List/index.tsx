b567edef29b06713c72e8716df8bbeb73b1adb43
Daniel Silva <daniel.mascr@gmail.com>
2022-06-09 11:55:04 -0300

    Product listing from backend

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,17 +1,30 @@
+import { AxiosRequestConfig } from 'axios';
 import ProductCrudCard from 'pages/Admin/Products/ProductCrudCard';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Product } from 'types/product';
+import { SpringPage } from 'types/vendor/spring';
+import { requestBackend } from 'utils/requests';
 
 import './styles.css';
 
 const List = () => {
-  const product = {
-    id: 1,
-    name: 'Hamburger',
-    description: 'Hamburger de siri',
-    price: 35.0,
-    imgUri:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-  };
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/products',
+      params: {
+        page: 0,
+        size: 50,
+      },
+    };
+
+    requestBackend(config).then((response) => {
+      setPage(response.data);
+    });
+  }, []);
 
   return (
     <div className="product-crud-container">
@@ -24,15 +37,11 @@ const List = () => {
         <div className="base-card product-filter-container">Search bar</div>
       </div>
       <div className="row">
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
+        {page?.content.map((product) => (
+          <div className="col-sm-6 col-md-12" key={product.id}>
+            <ProductCrudCard product={product} />
+          </div>
+        ))}
       </div>
     </div>
   );
