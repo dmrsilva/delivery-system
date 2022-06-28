235d4b0f183189823d37c8fd7f4d0cc2fb0bafb5
Daniel Silva <daniel.mascr@gmail.com>
2022-06-28 12:09:15 -0300

    Netlify _redirects

--- frontweb/src/utils/requests.ts
+++ frontweb/src/utils/requests.ts
@@ -3,10 +3,11 @@ import { getAuthData } from './storage';
 import qs from 'qs';
 import history from './history';
 
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL =
+  process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
-const CLIENT_ID = 'dsdelivery';
-const CLIENT_SECRET = 'dsdelivery123';
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dsdelivery';
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dsdelivery123';
 
 type LoginData = {
   username: string;
